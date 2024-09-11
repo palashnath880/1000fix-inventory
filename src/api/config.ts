184119452f4d84f6e468909f9a53894a73157212
Palash Nath <palashnath880@gmail.com>
2024-09-11 13:01:29 +0600

    API config file added

---
+++ src/api/config.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_APP_API_URL,
+  headers: {
+    Authorization:
+      Cookies.get("auth_token") && `Bearer ${Cookies.get("auth_token")}`,
+    "Content-Type": "application/json",
+  },
+});
+
+export default instance;
