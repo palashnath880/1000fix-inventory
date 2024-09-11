9a6285d96b6493b72afb85d112a37c212ad673a3
Palash Nath <palashnath880@gmail.com>
2024-09-11 23:56:31 +0600

    redux store updated

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import branchReducer from "../features/branchSlice";
 import userReducer from "../features/userSlice";
+import skuCodeReducer from "../features/skuCodeSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     branches: branchReducer,
     users: userReducer,
+    skuCodes: skuCodeReducer,
   },
 });
 
