6933a6e039c5bd5c8305b6213cb498ceb12ae6a1
Palash Nath <palashnath880@gmail.com>
2024-09-11 19:53:39 +0600

    Branch api updated

--- src/api/branch.ts
+++ src/api/branch.ts
@@ -4,6 +4,9 @@ const branchApi = {
   create: (data: { name: string; address: string }) =>
     instance.post(`/branch`, data),
   get: (search: string = "") => instance.get(`/branch?search=${search}`),
+  update: (id: string, data: { address: string; users: string[] }) =>
+    instance.put(`/branch/${id}`, data),
+  delete: (id: string) => instance.delete(`/branch/${id}`),
 };
 
 export default branchApi;
