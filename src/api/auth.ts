fa5d460635d94c6949f4eff70b18a8445e178247
Palash Nath <palashnath880@gmail.com>
2024-09-11 12:41:22 +0600

    auth api updated

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,7 +3,7 @@ import instance from "./config";
 const authApi = {
   login: (data: { login: string; password: string }) =>
     instance.post(`/auth/login`, data),
-  verify: () => instance.post(`/auth/verify`),
+  verify: () => instance.post(`/auth/user`),
 };
 
 export default authApi;
