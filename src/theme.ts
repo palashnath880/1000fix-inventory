35c0a16f8f7821de4158dbd68c9d4e0292bf7b0d
Palash Nath <palashnath880@gmail.com>
2024-09-11 13:02:40 +0600

    MUI theme updated

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,33 @@
 import { createTheme } from "@mui/material/styles";
 
-const myTheme = createTheme({});
+const myTheme = createTheme({
+  typography: {
+    fontSize: 13,
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        style: {
+          paddingTop: 10,
+          paddingBottom: 10,
+          textTransform: "capitalize",
+        },
+      },
+    },
+  },
+  palette: {
+    primary: {
+      main: "#031C30",
+    },
+    secondary: {
+      main: "#f8f8ff",
+    },
+    text: {
+      // secondary: "#f8f8ff",
+      secondary: "#031C30",
+      primary: "#031C30",
+    },
+  },
+});
 
 export default myTheme;
