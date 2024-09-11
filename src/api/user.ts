f8614439ba7e3726f0ebb980557d4327a5f8c8e3
Palash Nath <palashnath880@gmail.com>
2024-09-11 20:44:20 +0600

    user api added

--- src/api/user.ts
+++ src/api/user.ts
@@ -4,6 +4,7 @@ const userApi = {
   create: (data: { name: string; email: string; role: string }) =>
     instance.post(`/user`, data),
   get: (search: string = "") => instance.get(`/user?search=${search}`),
+  delete: (id: string) => instance.delete(`/user/${id}`),
 };
 
 export default userApi;
