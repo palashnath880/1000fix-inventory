0971de11d6d04a5c4fd83dc79e3ba4bd24a2cbf3
Palash Nath <palashnath880@gmail.com>
2024-09-12 19:13:50 +0600

    stock api added

---
+++ src/api/stock.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import instance from "./config";
+
+const stockApi = {
+  entry: (data: any) => instance.post(`/stock/entry`, data),
+};
+
+export default stockApi;
