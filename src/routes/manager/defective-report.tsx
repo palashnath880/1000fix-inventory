9587116cf9891c4c0ee6e3d146b53cabeeca17e2
Palash Nath <palashnath880@gmail.com>
2024-09-22 21:07:00 +0600

    vercel.json file added

---
+++ src/routes/manager/defective-report.tsx
@@ -0,0 +1,9 @@
+import { Header } from "../../components/shared/TopBar";
+
+export default function DefectiveReport() {
+  return (
+    <div className="pb-10">
+      <Header title="Defective Report" />
+    </div>
+  );
+}
