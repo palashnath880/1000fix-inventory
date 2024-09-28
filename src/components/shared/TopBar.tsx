7b3295ef2af1ee2252977cb7664ed0b9189a45cf
Palash Nath <palashnath880@gmail.com>
2024-09-28 18:36:46 +0600

    topbar component update

--- src/components/shared/TopBar.tsx
+++ src/components/shared/TopBar.tsx
@@ -39,7 +39,7 @@ export default function TopBar() {
     <div className="px-5 py-2 shadow-lg bg-slate-100">
       <div className="flex items-center justify-between gap-4">
         <div className="flex flex-col">
-          <Typography className="!font-semibold !text-lg">{title}</Typography>
+          <Typography className="!font-medium !text-lg">{title}</Typography>
           {subtitle && <Typography variant="body2">{subtitle}</Typography>}
         </div>
         <div className="flex items-center gap-3">
