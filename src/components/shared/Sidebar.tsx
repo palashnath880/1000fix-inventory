d9c4c9730886f243000fa5ac1f040aa5bdc864d8
Palash Nath <palashnath880@gmail.com>
2024-09-28 18:21:42 +0600

    sidebar updated

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -217,7 +217,7 @@ export default function Sidebar() {
   ];
 
   return (
-    <aside className="h-full bg-primary px-4 py-5 overflow-y-auto overflow-x-hidden w-[260px]">
+    <aside className="h-full bg-primary px-4 py-5 overflow-y-auto overflow-x-hidden w-[260px] scrollbar">
       <div className="h-full flex flex-col gap-5">
         <div className="flex flex-col items-center">
           <img className="" src="" />
