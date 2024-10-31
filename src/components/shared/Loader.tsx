0c2a65a3c26e8b790fa6ed5d33c2c8504404a182
Palash Nath <palashnath880@gmail.com>
2024-10-31 11:42:26 +0600

    Loader component updated

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -1,3 +1,9 @@
+import img from "../../assets/logo.webp";
+
 export default function Loader() {
-  return <div className="w-screen h-screen grid place-items-center"></div>;
+  return (
+    <div className="w-screen h-screen grid place-items-center">
+      <img src={img} draggable={false} className="animate-pulse w-40" />
+    </div>
+  );
 }
