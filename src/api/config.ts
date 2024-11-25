dbea7b665d00677908735266712b8d0240205c75
Palash Nath <palashnath880@gmail.com>
2024-11-25 10:21:10 +0600

    update axios config file

--- src/api/config.ts
+++ src/api/config.ts
@@ -4,10 +4,20 @@ import Cookies from "js-cookie";
 const instance = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
   headers: {
-    Authorization:
-      Cookies.get("auth_token") && `Bearer ${Cookies.get("auth_token")}`,
     "Content-Type": "application/json",
   },
 });
+
+// set request authorization
+instance.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("auth_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default instance;
